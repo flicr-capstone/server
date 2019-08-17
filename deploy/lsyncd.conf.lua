d814e085b2ba492e4352b779ab05d877a62aa760
Michael Goodnow <mmgoodnow@gmail.com>
2019-08-17 13:19:59 -0400

    Generify lsyncd conf and ignore files

--- deploy/lsyncd.conf.lua
+++ deploy/lsyncd.conf.lua
@@ -1,14 +1,21 @@
 settings {
-        nodaemon = true
+	nodaemon = true
 }
 
 sync {
-        default.rsyncssh, 
-        source="./",
-        host="joe",
-        targetdir="./server",
-        delay = 1,
-        rsync = {
-            cvs_exclude = true
-        }
-    }
+	default.rsyncssh,
+	source = ".",
+	host = "pi@joe.local",
+	targetdir = "server",
+	delay = 1,
+	excludeFrom = "deploy/.lsyncd-ignore";
+	rsync = {
+		cvs_exclude = true,
+--		binary = "/usr/local/bin/rsync",
+		_extra = "--filter=':- .gitignore'"
+	},
+	ssh = {
+--		binary = "/usr/local/bin/ssh",
+--		identityFile = "/path/to/.ssh/id_rsa"
+	}
+}
