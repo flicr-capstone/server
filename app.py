7c7c97e04efb6b8e12a69585134b90115521decb
James Herbstritt <james.herbstritt@gmail.com>
2019-08-24 01:30:46 -0400

    issue-10: Complete POC for websockets

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import asyncio
+import websockets
 
 app = Flask(__name__)
 
@@ -6,3 +8,26 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return "Hello World!"
+
+
+async def hello(websocket, path):
+	while True:
+		name = await websocket.recv()
+
+		print(f"< {name}")
+
+		greeting = f"Hello {name}!"
+
+		await websocket.send(greeting)
+		print(f"> {greeting}")
+
+
+def main():
+	start_server = websockets.serve(hello, "localhost", 8080)
+
+	asyncio.get_event_loop().run_until_complete(start_server)
+	asyncio.get_event_loop().run_forever()
+
+
+if __name__ == "__main__":
+	main()
