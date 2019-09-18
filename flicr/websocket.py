a9a401177ce40c9937cba7d31ae3e80a34d2505c
Michael Goodnow <mmgoodnow@gmail.com>
2019-09-17 21:17:57 -0400

    Set up websocket triage

---
+++ flicr/websocket.py
@@ -0,0 +1,31 @@
+import asyncio
+import json
+import websockets
+
+from .payloadtype import PayloadType
+
+
+async def triage(ws, _path):
+    while True:
+        payload = json.loads(await ws.recv())
+        payload_type = PayloadType(payload["type"])
+        if payload_type == PayloadType.GREET:
+            await greet(ws, payload["msg"])
+
+
+async def send(ws, payload_type, msg):
+    await ws.send(json.dumps({"type": payload_type, "msg": msg}))
+
+
+async def greet(ws, greeting):
+    print(f"< {greeting}")
+    response = {"greeting": f"Hello {greeting['name']}!"}
+    await send(ws, PayloadType.GREET, response)
+    print(f"> {response}")
+
+
+def run():
+    print("Starting WebSocket Server")
+    start_server = websockets.serve(triage, "localhost", 8080)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
