4588f73233df20adf788e36481941d2e21ded978
Michael Goodnow <mmgoodnow@gmail.com>
2020-03-06 19:13:27 -0500

    Add gstreamer and janus services (#10)

--- deploy/install.sh
+++ deploy/install.sh
@@ -4,12 +4,12 @@ if [[ "$EUID" -ne 0 ]]
   then echo "Please run with sudo"
   exit
 fi
-
-systemctl stop flicr
-cp deploy/flicr.service /lib/systemd/system
+services="flicr gstreamer janus"
+systemctl stop $services
+cp deploy/*.service /lib/systemd/system
 systemctl daemon-reload
-systemctl enable flicr
-systemctl start flicr
+systemctl enable $services
+systemctl start $services
 
 cp deploy/joe.local /etc/nginx/sites-available
 ln -s ../sites-available/joe.local /etc/nginx/sites-enabled/joe.local
