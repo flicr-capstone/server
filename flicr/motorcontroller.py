ddddf61b968d22e01b4b92792d76216217e9679f
Michael Goodnow <mmgoodnow@gmail.com>
2020-03-13 01:36:53 -0400

    Reverse proxy to the websocket server (#12)

--- flicr/motorcontroller.py
+++ flicr/motorcontroller.py
@@ -1,14 +1,15 @@
 import itertools
 import serial
 
+
 class MotorController:
     def __init__(self, port, baud_rate=9600):
         try:
             self.connection = serial.Serial(port, baud_rate)
         except Exception as e:
-            raise Exception(f'Error opening port {port} Error: {e}')
+            raise Exception(f"Error opening port {port} Error: {e}")
 
-    ## Arguments to build packet -> Number of bytes written (int)
+    # Arguments to build packet -> Number of bytes written (int)
     def write(self, header, num_id, length, cmd, params, checksum):
         args = list(header, num_id, length, cmd, params, checksum)
         flattened_args = list(itertools.chain.from_iterable(args))
--- flicr/payloadtype.py
+++ flicr/payloadtype.py
@@ -3,3 +3,4 @@ from enum import Enum
 
 class PayloadType(str, Enum):
     GREET = "GREET"
+    KEY_EVENT = "KEY_EVENT"
